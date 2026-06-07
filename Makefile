68d0d607321d0c608cf3a6bf0166afbbb57524e0
ameine <209898262+a-meine@users.noreply.github.com>
2026-06-07 10:05:16 +0200

    adding Makefile, default config and main

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+sources = $(a-maze-ing.py)
+
+
+all:
+	python3 a_maze_ing.py config.txt
---
+++ a_maze_ing.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+from maze.config_parser2 import load_config
+
+
+if __name__ == "__main__":
+    if len(sys.argv[1:]) != 1:
+        print("No config file were given")
+        print("Usage: python3 a_maze_ing.py config.txt")
+        exit(0)
+
+    config_file: str = sys.argv[1]
+    try:
+        Maze_config = load_config(config_file)
+        print("parsed config parameters: ")
+        print(Maze_config)
+    except (KeyError, ValueError, TypeError) as e:
+        print(e)
+
